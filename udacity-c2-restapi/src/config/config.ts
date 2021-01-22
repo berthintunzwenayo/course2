a4be7c0d723c0d238b6efc3d10ef261c760679a6
berthintunzwenayo <berthintunzwenayo95@gmail.com>
2021-01-22 14:31:02 +0200

    edited

---
+++ udacity-c2-restapi/src/config/config.ts
@@ -0,0 +1,19 @@
+export const config = {
+    "dev": {
+    "username": "mdrdsinstance",
+    "password": "mdrdsinstance",
+    "database": "mdrdsinstance",
+    "host": "mdrdsinstance.cx9rlx1wfnfq.us-east-2.rds.amazonaws.com",
+    "dialect": "postgress",
+    "aws_region": "us-east-2",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagram-ruttner-dev"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
