42910d74c17ddd44dcec95787ffbc7e7b7a5ffec
berthintunzwenayo <berthintunzwenayo95@gmail.com>
2021-01-23 12:05:43 +0200

    new

--- udacity-c2-restapi/src/config/config.ts
+++ udacity-c2-restapi/src/config/config.ts
@@ -1,10 +1,10 @@
 export const config = {
     "dev": {
-    "username": "mdrdsinstance",
-    "password": "mdrdsinstance",
-    "database": "mdrdsinstance",
-    "host": "mdrdsinstance.cx9rlx1wfnfq.us-east-2.rds.amazonaws.com",
-    "dialect": "postgress",
+        "username": "postgres",
+        "password": "rdschallengedme",
+        "database": "md_database",
+        "host": "rds-posgresql-exercise-md.cx9rlx1wfnfq.us-east-2.rds.amazonaws.com",
+    "dialect": "postgres",
     "aws_region": "us-east-2",
     "aws_profile": "default",
     "aws_media_bucket": "udagram-ruttner-dev"
